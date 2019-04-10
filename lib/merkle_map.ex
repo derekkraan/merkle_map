801d4b9e750cbc2455ed48cb87184db39f301ff6
Derek Kraan <derek.kraan@gmail.com>
2019-04-10 10:35:32 +0200

    Initial commit

---
+++ lib/merkle_map.ex
@@ -0,0 +1,18 @@
+defmodule MerkleMap do
+  @moduledoc """
+  Documentation for MerkleMap.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MerkleMap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/merkle_map_test.exs
@@ -0,0 +1,8 @@
+defmodule MerkleMapTest do
+  use ExUnit.Case
+  doctest MerkleMap
+
+  test "greets the world" do
+    assert MerkleMap.hello() == :world
+  end
+end
