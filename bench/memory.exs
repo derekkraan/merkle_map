3dcfe5df3214d165956b9f5fcf4cdacaa578cb55
Derek Kraan <derek.kraan@gmail.com>
2019-04-13 17:09:08 +0200

    Add syncing in stages to memory benchmark

--- bench/memory.exs
+++ bench/memory.exs
@@ -4,9 +4,27 @@ defmodule MemoryHelper do
   end
 end
 
-mm = MerkleMap.new(1..10000, fn x -> {x, x} end)
+mm1 = MerkleMap.new(1..10000, fn x -> {x, x} end)
 
-IO.inspect({FullMerkleMap, MemoryHelper.memory_kb(mm)})
+mm2 = MerkleMap.new(2..10001, fn x -> {x, x} end)
+
+IO.inspect({FullMerkleMap, MemoryHelper.memory_kb(mm1)})
 
 m = Map.new(1..10000, fn x -> {x, x} end)
+
 IO.inspect({Map, MemoryHelper.memory_kb(m)})
+
+Enum.sort([1, 10001]) == Enum.sort(MerkleMap.diff_keys(mm1, mm2))
+
+{:continue, first_partial} = MerkleMap.prepare_partial_diff(mm1, 8)
+{:continue, second_partial} = MerkleMap.diff_keys(first_partial, mm2, 8)
+{:continue, third_partial} = MerkleMap.diff_keys(second_partial, mm1, 8)
+{:continue, fourth_partial} = MerkleMap.diff_keys(third_partial, mm2, 8)
+
+IO.inspect(
+  {MerkleMapSyncInStages,
+   Enum.map(
+     [first_partial, second_partial, third_partial, fourth_partial],
+     &MemoryHelper.memory_kb/1
+   )}
+)
