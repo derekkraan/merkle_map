76fff2d58349ce9efd39727296ff0d96f54510b4
Derek Kraan <derek.kraan@gmail.com>
2019-04-14 10:17:20 +0200

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MerkleMap.MixProject do
   def project do
     [
       app: :merkle_map,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       package: package(),
