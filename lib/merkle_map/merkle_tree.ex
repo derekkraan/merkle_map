3a398b8bc6476b1147fbfa76b384884fee8da9b5
Derek Kraan <derek.kraan@gmail.com>
2019-04-14 09:59:07 +0200

    Optimize over-the-wire message sizes. `[]` is 1 byte, while `nil` is 6.

--- lib/merkle_map/merkle_tree.ex
+++ lib/merkle_map/merkle_tree.ex
@@ -1,5 +1,5 @@
 defmodule MerkleMap.MerkleTree do
-  defstruct object: {"", nil, nil}
+  defstruct object: {"", [], []}
 
   def new() do
     %__MODULE__{}
@@ -27,9 +27,9 @@ defmodule MerkleMap.MerkleTree do
   def keys({_hash, :partial}), do: ["partial"]
   def keys({_, a, b}), do: keys(a) ++ keys(b)
   def keys({_, values}), do: Map.keys(values)
-  def keys(nil), do: []
+  def keys([]), do: []
 
-  def all_leaves(nil, _loc), do: []
+  def all_leaves([], _loc), do: []
 
   def all_leaves({_, a, b}, loc) do
     all_leaves(a, <<loc::bitstring, 0::1>>) ++ all_leaves(b, <<loc::bitstring, 1::1>>)
@@ -45,22 +45,22 @@ defmodule MerkleMap.MerkleTree do
     diff_keys(t1, t2, loc)
   end
 
-  def diff_keys(nil, nil, _loc), do: []
+  def diff_keys([], [], _loc), do: []
 
   def diff_keys({h, _, _}, {h, _, _}, _loc) do
     []
   end
 
-  def diff_keys(nil, {_, _, _} = tree, loc),
+  def diff_keys([], {_, _, _} = tree, loc),
     do: all_leaves(tree, loc)
 
-  def diff_keys({_, _, _} = tree, nil, loc),
+  def diff_keys({_, _, _} = tree, [], loc),
     do: all_leaves(tree, loc)
 
   def diff_keys({_, :partial}, _, loc), do: [{:partial, loc}]
   def diff_keys(_, {_, :partial}, loc), do: [{:partial, loc}]
 
-  def diff_keys(nil, {_, values}, _loc), do: Map.keys(values)
+  def diff_keys([], {_, values}, _loc), do: Map.keys(values)
 
   def diff_keys({_, a1, a2}, {_, b1, b2}, loc) do
     diff_keys(a1, b1, <<loc::bitstring, 0::1>>) ++ diff_keys(a2, b2, <<loc::bitstring, 1::1>>)
@@ -72,7 +72,7 @@ defmodule MerkleMap.MerkleTree do
     |> Enum.reject(fn x -> Map.get(v1, x) == Map.get(v2, x) end)
   end
 
-  def prune_empty_nodes({_, nil, nil}), do: nil
+  def prune_empty_nodes({_, [], []}), do: []
   def prune_empty_nodes(tree), do: tree
 
   def calculate_hash(%__MODULE__{object: {_, a, b}}) do
@@ -89,7 +89,7 @@ defmodule MerkleMap.MerkleTree do
 
   defp get_hash({h, _values}), do: h
   defp get_hash({h, _a, _b}), do: h
-  defp get_hash(nil), do: ""
+  defp get_hash([]), do: ""
 
   def partial_tree(tree, loc, levels)
 
@@ -97,7 +97,7 @@ defmodule MerkleMap.MerkleTree do
     %__MODULE__{object: partial_tree(tree, loc, levels)}
   end
 
-  def partial_tree(nil, _, _), do: nil
+  def partial_tree([], _, _), do: []
 
   def partial_tree({_, a, _b}, <<0::1, rest_loc::bits>>, levels),
     do: partial_tree(a, rest_loc, levels)
@@ -124,7 +124,7 @@ defmodule MerkleMap.MerkleTree do
 
   def delete(tree, key) do
     case delete(tree, location(key), key) do
-      nil -> %__MODULE__{} |> calculate_hash()
+      [] -> %__MODULE__{} |> calculate_hash()
       tree -> tree
     end
   end
@@ -146,7 +146,7 @@ defmodule MerkleMap.MerkleTree do
     new_values = Map.delete(values, key)
 
     if Map.size(new_values) == 0 do
-      nil
+      []
     else
       {hash(new_values), new_values}
     end
@@ -163,13 +163,13 @@ defmodule MerkleMap.MerkleTree do
   defp put(tree, <<0::size(1), rest_loc::bits>>, k, v) do
     {_hash, a, b} = init_empty_inner_node(tree)
     new_a = put(a, rest_loc, k, v)
-    {nil, new_a, b} |> calculate_hash()
+    {[], new_a, b} |> calculate_hash()
   end
 
   defp put(tree, <<1::size(1), rest_loc::bits>>, k, v) do
     {_hash, a, b} = init_empty_inner_node(tree)
     new_b = put(b, rest_loc, k, v)
-    {nil, a, new_b} |> calculate_hash()
+    {[], a, new_b} |> calculate_hash()
   end
 
   defp put(tree, <<>>, key, value) do
@@ -179,8 +179,8 @@ defmodule MerkleMap.MerkleTree do
   end
 
   defp init_empty_leaf({_hash, _value} = tree), do: tree
-  defp init_empty_leaf(nil), do: {nil, %{}}
+  defp init_empty_leaf([]), do: {[], %{}}
 
   defp init_empty_inner_node({_hash, _a, _b} = tree), do: tree
-  defp init_empty_inner_node(nil), do: {nil, nil, nil}
+  defp init_empty_inner_node([]), do: {[], [], []}
 end
