e9018c72f681d886e61fe7f02bf4958601dc12e7
Guilherme Balena Versiani <guilherme@telnyx.com>
2020-05-13 18:39:13 -0300

    Split the MerkleTree logic from the MerkleMap, so the MerkleTree alone gets reusable without bringing the Map with it.

--- lib/merkle_map/merkle_tree_diff.ex
+++ lib/merkle_map/merkle_tree_diff.ex
@@ -2,4 +2,10 @@ defmodule MerkleMap.MerkleTree.Diff do
   @moduledoc false
 
   defstruct trees: [], keys: []
+
+  def truncate_diff(%__MODULE__{} = diff, amount) do
+    keys = Enum.take(diff.keys, amount)
+    trees = Enum.take(diff.trees, amount - length(keys))
+    %{diff | keys: keys, trees: trees}
+  end
 end
